36854954c652c2b34771163d3cd49df95911b147
nikhilagrawal2121 <104684468+nikhilagrawal2121@users.noreply.github.com>
2022-05-02 19:29:04 +0530

    commit

--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -6,9 +6,9 @@ public class LevelSelector : MonoBehaviour
     public SceneFader fader; //Refrance to scene
     public Button[] LevelButtons;
 
-
     private void Start()
     {
+     
         int LevelReached = PlayerPrefs.GetInt("LevelReached",1); //used to find which is our current level is we have not play any level then it will be start of game
                                                                  // if we are new to game it only allows u to play first level
         for (int i = 0; i < LevelButtons.Length; i++) //read number of buttons
@@ -16,6 +16,8 @@ public class LevelSelector : MonoBehaviour
             if(i+1 > LevelReached) // if levelreched value is less then i 
             LevelButtons[i].interactable = false; // when we click nothing will be happened 
                                                   //we cannot play that level
+
+         
         }
              
     }
